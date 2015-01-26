992b91fce2d763ae7d0559690dbd46953f9dbd92
bodonoghue <bodonoghue@quantcast.com>
2015-01-26 14:42:22 +0000

    add actual test

---
+++ examples/matlab/power_cone_test.m
@@ -0,0 +1,28 @@
+clear all;
+randn('seed',0);
+n = 50;
+pow = pi;
+const = 1.5;
+a = randn(n,1);
+
+%% cvx, using SDP formulation
+cvx_begin
+variable x(n)
+minimize(norm(x, pow))
+subject to
+a'*x == const
+cvx_end
+
+%% scs, with power cone formulation
+data.A = [a' zeros(1,n) 0; zeros(1,n) ones(1,n) -1];
+for i=1:n
+    ei = zeros(n,1);ei(i) = -1;
+    data.A = [data.A; zeros(1,n) ei' 0; zeros(1,2*n) -1;  ei' zeros(1,n) 0];
+end
+data.A = sparse(data.A);
+data.c = [zeros(2*n,1); 1];
+data.b = [const; 0 ; zeros(3*n,1)];
+K = struct('f', 2, 'p', ones(n,1) / pow);
+[x_scs,y_scs,s_scs,info] = scs_indirect(data, K, {});
+
+norm(x-x_scs(1:n))
