63512401beebd9c026f123a5b7ebab557deef5fc
bodonoghue <bodonoghue@quantcast.com>
2015-05-28 10:45:48 +0100

    add debug to demo.c

--- examples/c/demo.c
+++ examples/c/demo.c
@@ -25,7 +25,6 @@ int main(int argc, char **argv) {
 	k = scs_calloc(1, sizeof(Cone));
 	d = scs_calloc(1, sizeof(Data));
 	sol = scs_calloc(1, sizeof(Sol));
-
 	if (readInData(fp, d, k) == -1) {
 		printf("Error reading in data, aborting.\n");
 		return -1;
@@ -61,95 +60,145 @@ int main(int argc, char **argv) {
 }
 
 scs_int readInData(FILE * fp, Data * d, Cone * k) {
-	/* MATRIX IN DATA FILE MUST BE IN COLUMN COMPRESSED FORMAT */
-	scs_int i, Anz;
-	AMatrix * A;
-	Settings * stgs = scs_malloc(sizeof(Settings));
-	stgs->rho_x = RHOX;
-	stgs->warm_start = 0;
-	stgs->scale = 1;
-	if (fscanf(fp, INTRW, &(d->n)) != 1)
-		return -1;
-	if (fscanf(fp, INTRW, &(d->m)) != 1)
-		return -1;
-	if (fscanf(fp, INTRW, &(k->f)) != 1)
-		return -1;
-	if (fscanf(fp, INTRW, &(k->l)) != 1)
+    /* MATRIX IN DATA FILE MUST BE IN COLUMN COMPRESSED FORMAT */
+    scs_int i, Anz;
+    AMatrix * A;
+    Settings * stgs = scs_malloc(sizeof(Settings));
+    stgs->rho_x = RHOX;
+    stgs->warm_start = 0;
+    stgs->scale = 1;
+    if (fscanf(fp, INTRW, &(d->n)) != 1) {
+        DEBUG_FUNC
         return -1;
-    if (fscanf(fp, INTRW, &(k->qsize)) != 1)
+    }
+    if (fscanf(fp, INTRW, &(d->m)) != 1) {
+        DEBUG_FUNC
         return -1;
-    if (fscanf(fp, INTRW, &(k->ssize)) != 1)
+    }
+    if (fscanf(fp, INTRW, &(k->f)) != 1) {
+        DEBUG_FUNC
         return -1;
-    if(fscanf(fp, INTRW, &(k->ep))!= 1)
+    }
+    if (fscanf(fp, INTRW, &(k->l)) != 1) {
+        DEBUG_FUNC
         return -1;
-    if(fscanf(fp, INTRW, &(k->ed))!= 1)
+    }
+    if (fscanf(fp, INTRW, &(k->qsize)) != 1) {
+        DEBUG_FUNC
         return -1;
-    if (fscanf(fp, INTRW, &(k->psize)) != 1)
+    }
+    if (fscanf(fp, INTRW, &(k->ssize)) != 1) {
+        DEBUG_FUNC
         return -1;
+    }
+    if (fscanf(fp, INTRW, &(k->ep))!= 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    if (fscanf(fp, INTRW, &(k->ed))!= 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    if (fscanf(fp, INTRW, &(k->psize)) != 1) {
+        DEBUG_FUNC
+        return -1;
+    }
 
-	if (fscanf(fp, INTRW, &(stgs->max_iters)) != 1)
-		return -1;
-	if (fscanf(fp, INTRW, &(stgs->verbose)) != 1)
-		return -1;
-	if (fscanf(fp, INTRW, &(stgs->normalize)) != 1)
-		return -1;
-	if (fscanf(fp, FLOATRW, &(stgs->alpha)) != 1)
-		return -1;
-	if (fscanf(fp, FLOATRW, &(stgs->eps)) != 1)
-		return -1;
-	if (fscanf(fp, FLOATRW, &(stgs->rho_x)) != 1)
-		return -1;
-	if (fscanf(fp, FLOATRW, &(stgs->scale)) != 1)
-		return -1;
-	if (fscanf(fp, FLOATRW, &(stgs->cg_rate)) != 1)
-		return -1;
-	k->q = malloc(sizeof(scs_int) * k->qsize);
-	for (i = 0; i < k->qsize; i++) {
-		if (fscanf(fp, INTRW, &k->q[i]) != 1)
-			return -1;
-	}
-	k->s = malloc(sizeof(scs_int) * k->ssize);
-	for (i = 0; i < k->ssize; i++) {
-		if (fscanf(fp, INTRW, &k->s[i]) != 1)
-			return -1;
-	}
-	k->p = malloc(sizeof(scs_float) * k->psize);
-	for (i = 0; i < k->psize; i++) {
-		if (fscanf(fp, FLOATRW, &k->p[i]) != 1)
-			return -1;
-	}
-	d->b = malloc(sizeof(scs_float) * d->m);
-	for (i = 0; i < d->m; i++) {
-		if (fscanf(fp, FLOATRW, &d->b[i]) != 1)
-			return -1;
-	}
-	d->c = malloc(sizeof(scs_float) * d->n);
-	for (i = 0; i < d->n; i++) {
-		if (fscanf(fp, FLOATRW, &d->c[i]) != 1)
-			return -1;
-	}
-	A = malloc(sizeof(AMatrix));
-	A->p = malloc(sizeof(scs_int) * (d->n + 1));
-	for (i = 0; i < d->n + 1; i++) {
-		if (fscanf(fp, INTRW, &A->p[i]) != 1)
-			return -1;
-	}
-	Anz = A->p[d->n];
-	A->i = malloc(sizeof(scs_int) * Anz);
-	for (i = 0; i < Anz; i++) {
-		if (fscanf(fp, INTRW, &A->i[i]) != 1)
-			return -1;
-	}
-	A->x = malloc(sizeof(scs_float) * Anz);
-	for (i = 0; i < Anz; i++) {
-		if (fscanf(fp, FLOATRW, &A->x[i]) != 1)
-			return -1;
-	}
-	A->n = d->n;
-	A->m = d->m;
-	d->A = A;
-	d->stgs = stgs;
-	return 0;
+    if (fscanf(fp, INTRW, &(stgs->max_iters)) != 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    if (fscanf(fp, INTRW, &(stgs->verbose)) != 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    if (fscanf(fp, INTRW, &(stgs->normalize)) != 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    if (fscanf(fp, FLOATRW, &(stgs->alpha)) != 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    if (fscanf(fp, FLOATRW, &(stgs->eps)) != 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    if (fscanf(fp, FLOATRW, &(stgs->rho_x)) != 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    if (fscanf(fp, FLOATRW, &(stgs->scale)) != 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    if (fscanf(fp, FLOATRW, &(stgs->cg_rate)) != 1) {
+        DEBUG_FUNC
+        return -1;
+    }
+    k->q = malloc(sizeof(scs_int) * k->qsize);
+    for (i = 0; i < k->qsize; i++) {
+        if (fscanf(fp, INTRW, &k->q[i]) != 1) {
+            DEBUG_FUNC
+            return -1;
+        }
+    }
+    k->s = malloc(sizeof(scs_int) * k->ssize);
+    for (i = 0; i < k->ssize; i++) {
+        if (fscanf(fp, INTRW, &k->s[i]) != 1) {
+            DEBUG_FUNC
+            return -1;
+        }
+    }
+    k->p = malloc(sizeof(scs_float) * k->psize);
+    for (i = 0; i < k->psize; i++) {
+        if (fscanf(fp, FLOATRW, &k->p[i]) != 1) {
+            DEBUG_FUNC
+            return -1;
+        }
+    }
+    d->b = malloc(sizeof(scs_float) * d->m);
+    for (i = 0; i < d->m; i++) {
+        if (fscanf(fp, FLOATRW, &d->b[i]) != 1) {
+            DEBUG_FUNC
+            return -1;
+        }
+    }
+    d->c = malloc(sizeof(scs_float) * d->n);
+    for (i = 0; i < d->n; i++) {
+        if (fscanf(fp, FLOATRW, &d->c[i]) != 1) {
+            DEBUG_FUNC
+            return -1;
+        }
+    }
+    A = malloc(sizeof(AMatrix));
+    A->p = malloc(sizeof(scs_int) * (d->n + 1));
+    for (i = 0; i < d->n + 1; i++) {
+        if (fscanf(fp, INTRW, &A->p[i]) != 1) {
+            DEBUG_FUNC
+            return -1;
+        }
+    }
+    Anz = A->p[d->n];
+    A->i = malloc(sizeof(scs_int) * Anz);
+    for (i = 0; i < Anz; i++) {
+        if (fscanf(fp, INTRW, &A->i[i]) != 1) {
+            DEBUG_FUNC
+            return -1;
+        }
+    }
+    A->x = malloc(sizeof(scs_float) * Anz);
+    for (i = 0; i < Anz; i++) {
+        if (fscanf(fp, FLOATRW, &A->x[i]) != 1) {
+            DEBUG_FUNC
+            return -1;
+        }
+    }
+    A->n = d->n;
+    A->m = d->m;
+    d->A = A;
+    d->stgs = stgs;
+    return 0;
 }
 
 scs_int openFile(scs_int argc, char ** argv, scs_int idx, char * default_file, FILE ** fb) {
