cddee3b0b5517e30d0eb9492ca1fa1db3f690cd6
bodonoghue <bodonoghue@quantcast.com>
2015-06-20 13:01:33 +0100

    remove use_indirect param since not used yet

--- r/R/scs.R
+++ r/R/scs.R
@@ -5,13 +5,12 @@
 #' @param c primal objective vector
 #' @param cone list of cone sizes
 #' @param params solver parameters
-#' @param use_indirect boolean to use the indirect solver 
 #' @return list of solution vectors x,y,s and information about run
 #' @seealso \code{\link{nchar}} which this function wraps
 #' @export
 #' @examples
 #' scs(matrix(c(0.5,2),2,1), c(3, 1), 1, list(l=2), list(max_iters=5000))
-scs <- function(A, b, c, cone, params, use_indirect=false) {
+scs <- function(A, b, c, cone, params) {
         As <- as(A, "dgCMatrix")
         data <- list(m = dim(A)[1], n = dim(A)[2], Ax = As@x, Ai = As@i, Ap = As@p, b = b, c = c)
         ret <- .Call("scsr", data, cone, params, PACKAGE="scs")
