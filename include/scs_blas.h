26e2504c7cb9c772ed31039ed58d0b12eea66875
bodonoghue <bodonoghue@quantcast.com>
2015-07-24 16:44:18 +0100

    revert some BLASSUFFIX changes since it did not fix issue

--- include/scs_blas.h
+++ include/scs_blas.h
@@ -1,39 +1,26 @@
 #ifndef SCS_BLAS_H_GUARD
 #define SCS_BLAS_H_GUARD
 
+#ifdef LAPACK_LIB_FOUND
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/* check for empty, indirection required for stiching macros */
-#define _SCS_EMPTY_HELPER
-#define EMPTY(name) defined(name ## _SCS_EMPTY_HELPER)
-#define IS_EMPTY(name) EMPTY(name)
 
 /* Default to underscore for blas / lapack */
 #ifndef BLASSUFFIX
 #define BLASSUFFIX _
 #endif
 
-#ifdef LAPACK_LIB_FOUND
-    /* empty BLASSUFFIX macros are causing errors and warnings */
-    #if !IS_EMPTY(BLASSUFFIX)
-        /* this extra indirection is needed for BLASSUFFIX to work correctly as a variable */
-        #define stitch_(pre,x,post) pre ## x ## post
-        #define stitch__(pre,x,post) stitch_(pre,x,post)
-        /* single or double precision */
-        #ifndef FLOAT
-            #define BLAS(x) stitch__(d,x,BLASSUFFIX)
-        #else
-            #define BLAS(x) stitch__(s,x,BLASSUFFIX)
-        #endif
-    #else
-        #ifndef FLOAT
-            #define BLAS(x) a ## x
-        #else
-            #define BLAS(x) s ## x
-        #endif
-    #endif
+/* this extra indirection is needed for BLASSUFFIX to work correctly as a variable */
+#define stitch_(pre,x,post) pre ## x ## post
+#define stitch__(pre,x,post) stitch_(pre,x,post)
+
+/* single or double precision */
+#ifndef FLOAT
+    #define BLAS(x) stitch__(d,x,BLASSUFFIX)
+#else
+    #define BLAS(x) stitch__(s,x,BLASSUFFIX)
 #endif
 
 #ifdef MATLAB_MEX_FILE
@@ -48,4 +35,7 @@ typedef int blasint;
 #ifdef __cplusplus
 }
 #endif
-#endif
+
+#endif /* LAPACK_LIB_FOUND */
+
+#endif /* SCS_BLAS_H_GUARD */
