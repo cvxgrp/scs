8267f0037a602d493dbb18e71ed7c16c0c9e7c17
bodonoghue <bodonoghue@quantcast.com>
2016-01-22 22:32:10 +0000

    bug fix

--- matlab/make_scs.m
+++ matlab/make_scs.m
@@ -1,4 +1,4 @@
-gpu = false; % compile the gpu version of SCS
+gpu = true; % compile the gpu version of SCS
 float = false; % using single precision (rather than double) floating points
 int = false; % use 32 bit integers for indexing
 % WARNING: OPENMP WITH MATLAB CAN CAUSE ERRORS AND CRASH, USE WITH CAUTION:
@@ -29,8 +29,9 @@ else
 end
 
 if (float)
+    flags.LCFLAG = sprintf('-DFLOAT %s', flags.LCFLAG);
 end
-if (~int)
+if (int)
     flags.INT = '';
 else
     flags.INT = '-DDLONG';
