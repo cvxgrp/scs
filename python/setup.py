14c9613fe274b77edadd8a9e57f6ec866ab3a45c
bodonoghue <bodonoghue@quantcast.com>
2016-04-23 18:13:00 +0100

    still hacking at anaconda install

--- python/setup.py
+++ python/setup.py
@@ -54,7 +54,7 @@ def run_install():
     blas_info = get_info('blas_opt')
     # ugly hack due to scipy bug
     if 'libraries' in blas_info:
-        if 'mkl_lapack95_lp64' in blas_info['libraries']:
+        if 'mkl_intel_lp64' in blas_info['libraries']:
             blas_info = get_info('blas')
     if not blas_info:
         blas_info = get_info('blas')
@@ -63,7 +63,7 @@ def run_install():
     lapack_info = get_info('lapack_opt')
     # ugly hack due to scipy bug
     if 'libraries' in lapack_info:
-        if 'mkl_lapack95_lp64' in lapack_info['libraries']:
+        if 'mkl_intel_lp64' in lapack_info['libraries']:
             lapack_info = get_info('lapack')
     if not lapack_info:
         lapack_info = get_info('lapack')
