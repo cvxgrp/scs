be1f5358d3aa5ced9e9d4b20463303c8f01ba136
KrinaM <krinamnn@gmail.com>
2017-05-11 21:55:07 +0200

    created directions.h

---
+++ include/directions.h
@@ -0,0 +1,20 @@
+#ifndef DIRECTIONS_H
+#define DIRECTIONS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "scs.h"
+
+void computeLBroyden(Work *w, scs_int iter);
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DIRECTIONS_H */
+
