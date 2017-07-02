a5399eba370e5e90e3ec9ed88bbab8b5f1827468
KrinaM <krinamnn@gmail.com>
2017-07-02 18:11:33 +0200

    portfolio optimization  benchmark

---
+++ tests/matlab/benchmark_portfolio.m
@@ -0,0 +1,38 @@
+clear; clc;
+
+% Generate portfolio optimization problem
+rng('default');
+rng(1);
+
+density = 0.1;
+rc = 0.5; % estimated reciprocal condition number
+
+n = 100000;
+m = 100;
+
+mu = exp(0.01*randn(n,1))-1; % returns
+D = rand(n,1)/10; % idiosyncratic risk
+F = sprandn(n,m,density,rc)/10; % factor model
+gamma = 1;
+B = 1;
+
+%% 
+do_super_scs = 0;
+
+tic;
+cvx_begin
+    cvx_solver scs
+    cvx_solver_settings('eps', 1e-4,...
+        'scale', 1,...
+        'do_super_scs',do_super_scs,...
+        'direction', 100,...
+        'k0', 0,...
+        'memory', 100,...
+        'rho_x', 0.001,...
+        'verbose', 2)
+    variable x(n)
+    maximize(mu'*x - gamma*(sum_square(F'*x) + sum_square(D.*x)))
+    sum(x) == B
+    x >= 0
+cvx_end
+toc
