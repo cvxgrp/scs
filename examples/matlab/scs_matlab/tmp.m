af690e15bfe1daa015aa7f2853d46f95ebe7eed5
bodonoghue <bodonoghue@quantcast.com>
2017-09-04 21:03:31 +0100

    some experiments

--- examples/matlab/scs_matlab/tmp.m
+++ examples/matlab/scs_matlab/tmp.m
@@ -3,38 +3,54 @@ randn('seed',0)
 
 n= 100;
 
-A = randn(n); A = A*A*A;
+A = randn(n); A = A*A;
 b = randn(n,1);
 c = randn(n,1);
 lam = 1;
 
-%%
+use_indirect = 0;
 
+%%
+%{
 cvx_begin
 cvx_solver scs_matlab
-%cvx_solver('scs')
-cvx_solver_settings('anderson_lookback', 30, 'extra_verbose', 0, 'eps',1e-9, 'scale', 1, 'normalize', 1, 'use_indirect', 0, 'gen_plots', 1, 'max_iters', 100000)
+cvx_solver_settings('extra_verbose', 0, 'eps',1e-9, 'scale', 1, 'normalize', 1, 'use_indirect', use_indirect, 'gen_plots', 1, 'max_iters', 10000)
 variable x(n)
 minimize(norm(A*x - b) + lam*norm(x,1))
 x >= 0
 cvx_end
-
+%}
 %%
 
 cvx_begin
 cvx_solver scs_acc
 %cvx_solver('scs')
-cvx_solver_settings('anderson_lookback', 10, 'extra_verbose', 0, 'eps',1e-9, 'scale', 1, 'normalize', 1, 'use_indirect', 1, 'gen_plots', 1, 'max_iters', 100000)
+cvx_solver_settings('anderson_lookback', 20, 'extra_verbose', 0, 'eps',1e-9, 'scale', 1, 'normalize', 1, 'use_indirect', use_indirect, 'gen_plots', 1, 'max_iters', 10000)
 variable x(n)
 minimize(norm(A*x - b) + lam*norm(x,1))
 x >= 0
 cvx_end
 
 %%
+rmpath ~/Dropbox/research/superscs/matlab
+addpath ~/Dropbox/research/scs/matlab
 cvx_begin
 variable x(n)
+cvx_solver('scs')
+cvx_solver_settings('extra_verbose', 0, 'eps',1e-9, 'scale', 1, 'normalize', 1, 'use_indirect', use_indirect, 'gen_plots', 1, 'max_iters', 10000)
 minimize(norm(A*x - b) + lam*norm(x,1))
 x >= 0
 cvx_end
+
+rmpath ~/Dropbox/research/scs/matlab
+addpath ~/Dropbox/research/superscs/matlab
+cvx_begin
+variable x(n)
+cvx_solver('scs')
+cvx_solver_settings('extra_verbose', 0, 'eps',1e-9, 'scale', 1, 'normalize', 1, 'use_indirect', use_indirect, 'gen_plots', 1, 'max_iters', 10000)
+minimize(norm(A*x - b) + lam*norm(x,1))
+x >= 0
+cvx_end
+
 
 
