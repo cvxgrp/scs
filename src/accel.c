f0c65971b56192c73921536e4d43fbaa4c324a03
Brendan O'Donoghue <bodonoghue@google.com>
2017-09-26 15:11:25 +0100

    start normal form experiment

--- src/accel.c
+++ src/accel.c
@@ -12,7 +12,6 @@ struct SCS_ACCEL {
   scs_float *tmp;
   scs_float *dFQR;
   scs_float *wrk;
-  scs_float * dFTdFinv;
   scs_int k, l;
   blasint worksize;
 #endif
@@ -31,17 +30,22 @@ void BLAS(symv)(const char *uplo, const blasint *n,
 void BLAS(syr) (const char *uplo, const blasint *n,
                 const scs_float *alpha, const scs_float *x,
                 const blasint *incx, scs_float *a, const blasint *lda);
+void BLAS(syrk)(const char *uplo, const char *trans, blasint *n, blasint *k,
+                scs_float *alpha, scs_float *a, blasint *lda, scs_float *beta,
+                scs_float *c, blasint *ldc);
+void BLAS(posv) (const char *uplo, blasint * n, blasint * nrhs, scs_float * a,
+                 blasint * lda, scs_float * b, blasint * ldb, blasint * info);
+
+
 
 
 scs_int solve_accel_linsys(Accel *a);
 void update_accel_params(Work *w, scs_int idx);
-void rank_one_update(scs_float *A, scs_float *u, scs_int len, scs_float factor);
 
 void update_accel_params(Work *w, scs_int idx) {
   DEBUG_FUNC
   scs_float *dF = w->accel->dF;
   scs_float *dG = w->accel->dG;
-  scs_float *X = w->accel->dFTdFinv;
   scs_float *f = w->accel->f;
   scs_float *g = w->accel->g;
   scs_int l = w->m + w->n + 1;
@@ -63,7 +67,6 @@ void update_accel_params(Work *w, scs_int idx) {
     addScaledArray(&(dG[(idx - 1) * 2 * l]), g, 2 * l, -1);
     /* last col of dF = f_prev - f */
     addScaledArray(&(dF[(idx - 1) * 2 * l]), f, 2 * l, -1);
-    rank_one_update(X, &(dF[(idx - 1) * 2 * l]), 2 * l, 1.0);
   }
   RETURN;
 }
@@ -72,7 +75,6 @@ Accel *initAccel(Work *w) {
   DEBUG_FUNC
   Accel *a = scs_malloc(sizeof(Accel));
   scs_int l = w->m + w->n + 1;
-  scs_int i;
   if (!a) {
     RETURN SCS_NULL;
   }
@@ -94,10 +96,8 @@ Accel *initAccel(Work *w) {
   a->tmp = scs_malloc(sizeof(scs_float) * 2 * l);
   a->dFQR = scs_malloc(sizeof(scs_float) * 2 * l * a->k);
 
-  a->dFTdFinv = scs_calloc(a->k * a->k, sizeof(scs_float));
-  for (i = 0; i < a->k; ++i){
-    a->dFTdFinv[i * a->k + i] = 1000.0;
-  }
+  // TODO
+
 
   a->totalAccelTime = 0.0;
   /*
@@ -110,29 +110,6 @@ Accel *initAccel(Work *w) {
   RETURN a;
 }
 
-// factor should be one of -1, +1
-void rank_one_update(scs_float *A, scs_float *u, scs_int len, scs_float factor) {
-  DEBUG_FUNC
-  blasint blen = (blasint) len;
-  blasint one = (blasint) 1;
-  scs_float onef = 1.0;
-  scs_float zerof = 0.0;
-  printArray(A, len, 'A');
-  scs_printf("factor %f\n", factor);
-  scs_printf("len %i\n", len);
-  // TODO
-  scs_float * v = scs_malloc(sizeof(scs_float) * len);
-  // v = A * u
-  BLAS(symv)("Upper", &blen, &onef, A, &blen, u, &one, &zerof, v, &one);
-  // denom = 1 - factor * u ' * v
-  factor = factor / (1.0 - factor * innerProd(u, v, len));
-  // A = A - (factor / denom) *  v v'
-  BLAS(syr)("Upper", &blen, &factor, v, &one, A, &blen);
-  // TODO
-  scs_free(v);
-  RETURN;
-}
-
 scs_int solve_accel_linsys(Accel *a) {
   DEBUG_FUNC
   blasint twol = 2 * a->l;
@@ -141,43 +118,30 @@ scs_int solve_accel_linsys(Accel *a) {
   scs_float negOnef = -1.0;
   scs_float onef = 1.0;
   scs_float zerof = 0.0;
+  scs_int info;
+  scs_int i;
   // TODO
-  scs_float * out = scs_malloc(sizeof(scs_float) * k);
+  scs_float * X = scs_calloc(a->k * a->k, sizeof(scs_float));
+  for (i = 0; i < a->k; ++i) {
+    X[i * a->k + i] = 0.0001;
+  }
+  // now X = dF'*dF
+  BLAS(syrk)("Lower", "Transpose", &k, &twol, &onef, a->dF, &twol, &zerof, X, &k);
   // theta = dF' f
   BLAS(gemv)("Trans", &twol, &k, &onef, a->dF, &twol, a->f, &one, &zerof, a->theta, &one);
-  // out = (dF'dF)^-1 * theta
-  BLAS(symv)("Upper", &k, &onef, a->dFTdFinv, &k, a->theta, &one, &zerof, out, &one);
+  // theta = (dF'dF) \ dF' f
+  BLAS(posv)("Lower", &k, &one, X, &k, a->theta, &k, &info);
   memcpy(a->tmp, a->g, sizeof(scs_float) * 2 * a->l);
   // g = g - dG * theta
-  BLAS(gemv)("NoTrans", &twol, &k, &negOnef, a->dG, &twol, out, &one, &onef, a->tmp, &one);
+  BLAS(gemv)("NoTrans", &twol, &k, &negOnef, a->dG, &twol, a->theta, &one, &onef, a->tmp, &one);
   // TODO
-  scs_free(out);
+  scs_free(X);
   RETURN 0;
 }
-
-/*
-scs_int solve_accel_linsys(Accel *a) {
-  DEBUG_FUNC
-  blasint info;
-  scs_int l = a->l;
-  blasint twol = 2 * l;
-  blasint one = 1;
-  blasint k = (blasint)a->k;
-  scs_float negOnef = -1.0;
-  scs_float onef = 1.0;
-  memcpy(a->theta, a->f, sizeof(scs_float) * 2 * l);
-  memcpy(a->dFQR, a->dF, sizeof(scs_float) * 2 * l * k);
-  BLAS(gels)("NoTrans", &twol, &k, &one, a->dFQR, &twol, a->theta, &twol, a->wrk, &(a->worksize), &info);
-  memcpy(a->tmp, a->g, sizeof(scs_float) * 2 * l);
-  BLAS(gemv)("NoTrans", &twol, &k, &negOnef, a->dG, &twol, a->theta, &one, &onef, a->tmp, &one);
-  RETURN info;
-}
-*/
 
 scs_int accelerate(Work *w, scs_int iter) {
   DEBUG_FUNC
   scs_float *dF = w->accel->dF;
-  scs_float *dFTdFinv = w->accel->dFTdFinv;
   scs_float *dG = w->accel->dG;
   scs_float *tmp = w->accel->tmp;
   scs_int l = w->accel->l;
@@ -194,7 +158,6 @@ scs_int accelerate(Work *w, scs_int iter) {
   }
   if (iter > k) {
     /* shift dF */
-    rank_one_update(dFTdFinv, dF, 2 * l, -1.0);
     memmove(dF, &(dF[2 * l]), sizeof(scs_float) * 2 * l * (k - 1));
     /* shift dG */
     memmove(dG, &(dG[2 * l]), sizeof(scs_float) * 2 * l * (k - 1));
@@ -218,7 +181,6 @@ void freeAccel(Accel *a) {
     if (a->f) scs_free(a->f);
     if (a->g) scs_free(a->g);
     if (a->dFQR) scs_free(a->dFQR);
-    if (a->dFTdFinv) scs_free(a->dFTdFinv);
     if (a->theta) scs_free(a->theta);
     if (a->tmp) scs_free(a->tmp);
     if (a->wrk) scs_free(a->wrk);
