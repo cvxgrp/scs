17d15d7aafb038e68914c3d5c316fa496729b81d
Brendan O'Donoghue <bodonoghue@google.com>
2017-09-26 16:50:19 +0100

    remove memmove

--- src/accel.c
+++ src/accel.c
@@ -38,8 +38,32 @@ void BLAS(posv) (const char *uplo, blasint * n, blasint * nrhs, scs_float * a,
 
 /* TODO: rank 2 update, get rid of memove */
 
-scs_int solve_accel_linsys(Accel *a);
-void update_accel_params(Work *w, scs_int idx);
+scs_int solve_accel_linsys(Accel *a) {
+  DEBUG_FUNC
+  blasint twol = 2 * a->l;
+  blasint one = 1;
+  blasint k = (blasint)a->k;
+  scs_float negOnef = -1.0;
+  scs_float onef = 1.0;
+  scs_float zerof = 0.0;
+  scs_int info, i;
+  memset(a->X, 0, a->k * a->k * sizeof(scs_float));
+  if (ACCEL_REGULARIZATION > 0.) {
+    for (i = 0; i < a->k; ++i) {
+      a->X[i * a->k + i] = ACCEL_REGULARIZATION;
+    }
+  }
+  /* X = dF'*dF */
+  BLAS(syrk)("Lower", "Transpose", &k, &twol, &onef, a->dF, &twol, &onef, a->X, &k);
+  /* theta = dF' f */
+  BLAS(gemv)("Trans", &twol, &k, &onef, a->dF, &twol, a->f, &one, &zerof, a->theta, &one);
+  /* theta = (dF'dF) \ dF' f */
+  BLAS(posv)("Lower", &k, &one, a->X, &k, a->theta, &k, &info);
+  memcpy(a->tmp, a->g, sizeof(scs_float) * 2 * a->l);
+  /* g = g - dG * theta */
+  BLAS(gemv)("NoTrans", &twol, &k, &negOnef, a->dG, &twol, a->theta, &one, &onef, a->tmp, &one);
+  RETURN info;
+}
 
 void update_accel_params(Work *w, scs_int idx) {
   DEBUG_FUNC
@@ -48,12 +72,12 @@ void update_accel_params(Work *w, scs_int idx) {
   scs_float *f = w->accel->f;
   scs_float *g = w->accel->g;
   scs_int l = w->m + w->n + 1;
-  if (idx > 0) {
-    /* copy g_prev into idx col of dG */
-    memcpy(&(dG[(idx - 1) * 2 * l]), g, sizeof(scs_float) * 2 * l);
-    /* copy f_prev into idx col of dF */
-    memcpy(&(dF[(idx - 1) * 2 * l]), f, sizeof(scs_float) * 2 * l);
-  }
+  scs_int k = w->accel->k;
+  scs_int prev_idx = (k + idx - 1) % k;
+  /* copy g_prev into prev_idx col of dG */
+  memcpy(&(dG[prev_idx * 2 * l]), g, sizeof(scs_float) * 2 * l);
+  /* copy f_prev into idx col of dF */
+  memcpy(&(dF[prev_idx * 2 * l]), f, sizeof(scs_float) * 2 * l);
   /* g = [u;v] */
   memcpy(g, w->u, sizeof(scs_float) * l);
   memcpy(&(g[l]), w->v, sizeof(scs_float) * l);
@@ -61,12 +85,10 @@ void update_accel_params(Work *w, scs_int idx) {
   memcpy(f, g, sizeof(scs_float) * 2 * l);
   addScaledArray(f, w->u_prev, l, -1.0);
   addScaledArray(&(f[l]), w->v_prev, l, -1.0);
-  if (idx > 0) {
-    /* last col of dG = g_prev - g */
-    addScaledArray(&(dG[(idx - 1) * 2 * l]), g, 2 * l, -1);
-    /* last col of dF = f_prev - f */
-    addScaledArray(&(dF[(idx - 1) * 2 * l]), f, 2 * l, -1);
-  }
+  /* last col of dG = g_prev - g */
+  addScaledArray(&(dG[prev_idx * 2 * l]), g, 2 * l, -1);
+  /* last col of dF = f_prev - f */
+  addScaledArray(&(dF[prev_idx * 2 * l]), f, 2 * l, -1);
   RETURN;
 }
 
@@ -102,37 +124,8 @@ Accel *initAccel(Work *w) {
   RETURN a;
 }
 
-scs_int solve_accel_linsys(Accel *a) {
-  DEBUG_FUNC
-  blasint twol = 2 * a->l;
-  blasint one = 1;
-  blasint k = (blasint)a->k;
-  scs_float negOnef = -1.0;
-  scs_float onef = 1.0;
-  scs_float zerof = 0.0;
-  scs_int info, i;
-  memset(a->X, 0, a->k * a->k * sizeof(scs_float));
-  if (ACCEL_REGULARIZATION > 0.) {
-    for (i = 0; i < a->k; ++i) {
-      a->X[i * a->k + i] = ACCEL_REGULARIZATION;
-    }
-  }
-  /* X = dF'*dF */
-  BLAS(syrk)("Lower", "Transpose", &k, &twol, &onef, a->dF, &twol, &onef, a->X, &k);
-  /* theta = dF' f */
-  BLAS(gemv)("Trans", &twol, &k, &onef, a->dF, &twol, a->f, &one, &zerof, a->theta, &one);
-  /* theta = (dF'dF) \ dF' f */
-  BLAS(posv)("Lower", &k, &one, a->X, &k, a->theta, &k, &info);
-  memcpy(a->tmp, a->g, sizeof(scs_float) * 2 * a->l);
-  /* g = g - dG * theta */
-  BLAS(gemv)("NoTrans", &twol, &k, &negOnef, a->dG, &twol, a->theta, &one, &onef, a->tmp, &one);
-  RETURN info;
-}
-
 scs_int accelerate(Work *w, scs_int iter) {
   DEBUG_FUNC
-  scs_float *dF = w->accel->dF;
-  scs_float *dG = w->accel->dG;
   scs_float *tmp = w->accel->tmp;
   scs_int l = w->accel->l;
   scs_int k = w->accel->k;
@@ -142,18 +135,11 @@ scs_int accelerate(Work *w, scs_int iter) {
   if (k <= 0) {
     RETURN 0;
   }
+  /* update dF, dG, f, g */
+  update_accel_params(w, iter % k);
   if (iter < k) {
-    update_accel_params(w, iter);
     RETURN 0;
   }
-  if (iter > k) {
-    /* shift dF */
-    memmove(dF, &(dF[2 * l]), sizeof(scs_float) * 2 * l * (k - 1));
-    /* shift dG */
-    memmove(dG, &(dG[2 * l]), sizeof(scs_float) * 2 * l * (k - 1));
-  }
-  /* update dF, dG, f, g */
-  update_accel_params(w, k);
   /* solve linear system for theta */
   info = solve_accel_linsys(w->accel);
   /* set [u;v] = tmp */
