404e5d81bbd55d21fe99b47535a33c24577d9a18
Brendan O'Donoghue <bodonoghue@google.com>
2017-09-27 10:26:34 +0100

    make acceleration window rolling

--- src/accel.c
+++ src/accel.c
@@ -5,6 +5,16 @@
 /* Not clear if this should just be 0. */
 #define ACCEL_REGULARIZATION (0.)
 
+/* This package uses Anderson acceleration to improve the convergence of the
+ * ADMM iteration z^+ = \phi(z). At each iteration we need to solve a (small)
+ * linear system, we do this using LAPACK, first forming the normal equations
+ * and using ?posv (fastest, but bad numerical stability), if that fails we
+ * switch to using ?gels, which uses a QR factorization (slower, but better
+ * numerically). If this fails then we just don't do any acceleration this
+ * iteration, however we could fall back further to ?gelsy or other more
+ * robust methods if we really want
+ */
+
 struct SCS_ACCEL {
 #ifdef LAPACK_LIB_FOUND
   scs_float *dF;
@@ -37,58 +47,56 @@ void BLAS(gels)(const char *trans, const blasint *m, const blasint *n,
                 const blasint *lwork, blasint *info);
 
 /* TODO run clang format
- * allow rolling window on acceleration length (don't just start at k)
- * make sure variable memory is aligned when switching solver
  * make sure blasints all there
  */
 
-scs_int solve_with_gels(Accel * a) {
+scs_int solve_with_gels(Accel * a, scs_int len) {
   DEBUG_FUNC
   blasint info;
   scs_int l = a->l;
   blasint twol = 2 * l;
   blasint one = 1;
-  blasint k = (blasint) a->k;
+  blasint blen = (blasint) len;
   scs_float negOnef = -1.0;
   scs_float onef = 1.0;
   memcpy(a->theta, a->f, sizeof(scs_float) * 2 * l);
-  memcpy(a->X, a->dF, sizeof(scs_float) * 2 * l * k);
+  memcpy(a->X, a->dF, sizeof(scs_float) * 2 * l * len);
   /* solve dF theta = f */
-  BLAS(gels)("NoTrans", &twol, &k, &one, a->X, &twol, a->theta, &twol, a->wrk, &(a->worksize), &info);
+  BLAS(gels)("NoTrans", &twol, &blen, &one, a->X, &twol, a->theta, &twol, a->wrk, &(a->worksize), &info);
   /* sol = g */
   memcpy(a->sol, a->g, sizeof(scs_float) * 2 * l);
   /* sol = sol - dG * theta */
-  BLAS(gemv)("NoTrans", &twol, &k, &negOnef, a->dG, &twol, a->theta, &one, &onef, a->sol, &one);
+  BLAS(gemv)("NoTrans", &twol, &blen, &negOnef, a->dG, &twol, a->theta, &one, &onef, a->sol, &one);
   RETURN info;
 }
 
-scs_int solve_with_posv(Accel * a) {
+scs_int solve_with_posv(Accel * a, scs_int len) {
   DEBUG_FUNC
   scs_int i;
   blasint info;
   blasint twol = 2 * a->l;
   blasint one = 1;
-  blasint k = (blasint)a->k;
+  blasint blen = (blasint) len;
   scs_float negOnef = -1.0;
   scs_float onef = 1.0;
   scs_float zerof = 0.0;
   scs_float reg = ACCEL_REGULARIZATION;
-  memset(a->X, 0, a->k * a->k * sizeof(scs_float));
+  memset(a->X, 0, len * len * sizeof(scs_float));
   if (reg > 0.) {
-    for (i = 0; i < a->k; ++i) {
-      a->X[i * a->k + i] = 1.;
+    for (i = 0; i < len; ++i) {
+      a->X[i * len + i] = 1.;
     }
   }
   /* X = dF'*dF */
-  BLAS(syrk)("Lower", "Transpose", &k, &twol, &onef, a->dF, &twol, &reg, a->X, &k);
+  BLAS(syrk)("Lower", "Transpose", &blen, &twol, &onef, a->dF, &twol, &reg, a->X, &blen);
   /* theta = dF' f */
-  BLAS(gemv)("Trans", &twol, &k, &onef, a->dF, &twol, a->f, &one, &zerof, a->theta, &one);
+  BLAS(gemv)("Trans", &twol, &blen, &onef, a->dF, &twol, a->f, &one, &zerof, a->theta, &one);
   /* theta = (dF'dF) \ dF' f */
-  BLAS(posv)("Lower", &k, &one, a->X, &k, a->theta, &k, &info);
+  BLAS(posv)("Lower", &blen, &one, a->X, &blen, a->theta, &blen, &info);
   /* sol = g */
   memcpy(a->sol, a->g, sizeof(scs_float) * 2 * a->l);
   /* sol = sol - dG * theta */
-  BLAS(gemv)("NoTrans", &twol, &k, &negOnef, a->dG, &twol, a->theta, &one, &onef, a->sol, &one);
+  BLAS(gemv)("NoTrans", &twol, &blen, &negOnef, a->dG, &twol, a->theta, &one, &onef, a->sol, &one);
   RETURN (scs_int) info;
 }
 
@@ -102,6 +110,7 @@ scs_int init_gels(Accel * a){
   blasint one = 1;
   blasint k = (blasint) a->k;
   blasint worksize;
+  /* reuse variable names from posv */
   scs_free(a->X);
   scs_free(a->theta);
   a->theta = scs_malloc(sizeof(scs_float) * MAX(2 * a->l, a->k));
@@ -113,18 +122,18 @@ scs_int init_gels(Accel * a){
   RETURN (scs_int) info;
 }
 
-scs_int solve_accel_linsys(Accel *a) {
+scs_int solve_accel_linsys(Accel *a, scs_int len) {
   DEBUG_FUNC
   scs_int info;
   if (a->bad_numerics) {
-    RETURN solve_with_gels(a);
+    RETURN solve_with_gels(a, len);
   }
-  info = solve_with_posv(a);
+  info = solve_with_posv(a, len);
   if (info != 0) {
-    scs_printf("\tposv ran into numerical issues, info %i, switching to gels\n", info);
+    scs_printf("\tposv ran into numerical issues, info %i, switching to gels\n", (int) info);
     a->bad_numerics = 1;
     init_gels(a);
-    RETURN solve_with_gels(a);
+    RETURN solve_with_gels(a, len);
   }
   RETURN info;
 }
@@ -160,7 +169,7 @@ Accel *initAccel(Work *w) {
   if (!a) {
     RETURN SCS_NULL;
   }
-  a->l = w->m + w->n + 1;;
+  a->l = w->m + w->n + 1;
   a->k = MIN(2 * a->l, w->stgs->acceleration_lookback - 1);
   a->bad_numerics = 0;
   if (a->k <= 0) {
@@ -198,18 +207,20 @@ scs_int accelerate(Work *w, scs_int iter) {
   }
   /* update dF, dG, f, g */
   update_accel_params(w, (k + iter - 1) % k);
-  if (iter < k) {
+  /* iter < k doesn't do any acceleration until iters hit k */
+  /* if (iter < k) { */
+  if (iter == 0) {
     RETURN 0;
   }
   /* solve linear system, new point stored in sol */
-  info = solve_accel_linsys(w->accel);
+  info = solve_accel_linsys(w->accel, MIN(iter, k));
   /* set [u;v] = sol */
   if (info == 0) {
     memcpy(w->u, sol, sizeof(scs_float) * l);
     memcpy(w->v, &(sol[l]), sizeof(scs_float) * l);
   } else {
     /* Could fall back to gelsy or others in this case case */
-    scs_printf("\taccelerate error, info %i, no acceleration applied\n", info);
+    scs_printf("\taccelerate error, info %i, no acceleration applied\n", (int) info);
   }
   w->accel->totalAccelTime += tocq(&accelTimer);
   RETURN 0;
