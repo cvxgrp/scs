2ef3c5bbccf3188bfdfc9c3e6a274e27ff89ab58
Brendan O'Donoghue <bodonoghue@google.com>
2017-09-27 10:52:29 +0100

    cleanup python examples

--- examples/python/testAnderson.py
+++ examples/python/testAnderson.py
@@ -8,31 +8,46 @@ from genRandomConeProb import *
 #  Uses scs to solve a random cone problem  #
 #############################################
 
+
 def main():
-    #random.seed(0)
-    solveFeasible()
-    
+  #random.seed(0)
+  solveFeasible()
+
+
 def solveFeasible():
-    # cone:
-    K = {'f':10, 'l':15, 'q':[5, 10, 0 ,1], 's':[3, 4, 0, 0, 1], 'ep':10, 'ed':10, 'p':[-0.25, 0.5, 0.75, -0.33]}
-    m = getConeDims(K) 
-    data, p_star = genFeasible(K, n = m//3, density = 0.01)
-    params = {'normalize':True, 'scale':5, 'cg_rate':2,
-            'acceleration_lookback': 10}
-    
-    sol_i = scs.solve(data, K, use_indirect=True, **params)
-    xi = sol_i['x']
-    yi = sol_i['y']
-    print('p*  = ', p_star)
-    print('pri error = ', (dot(data['c'], xi) - p_star) / p_star)
-    print('dual error = ', (-dot(data['b'], yi) - p_star) / p_star)
-    # direct:
-    sol_d = scs.solve(data, K, use_indirect=False, **params)
-    xd = sol_d['x']
-    yd = sol_d['y']
-    print('p*  = ', p_star)
-    print('pri error = ', (dot(data['c'], xd) - p_star) / p_star)
-    print('dual error = ', (-dot(data['b'], yd) - p_star) / p_star)
-    
-if __name__ == "__main__":
-   main()
+  # cone:
+  K = {
+      'f': 10,
+      'l': 15,
+      'q': [5, 10, 0, 1],
+      's': [3, 4, 0, 0, 1],
+      'ep': 10,
+      'ed': 10,
+      'p': [-0.25, 0.5, 0.75, -0.33]
+  }
+  m = getConeDims(K)
+  data, p_star = genFeasible(K, n=m // 3, density=0.01)
+  params = {
+      'normalize': True,
+      'scale': 5,
+      'cg_rate': 2,
+      'acceleration_lookback': 10
+  }
+
+  sol_i = scs.solve(data, K, use_indirect=True, **params)
+  xi = sol_i['x']
+  yi = sol_i['y']
+  print('p*  = ', p_star)
+  print('pri error = ', (dot(data['c'], xi) - p_star) / p_star)
+  print('dual error = ', (-dot(data['b'], yi) - p_star) / p_star)
+  # direct:
+  sol_d = scs.solve(data, K, use_indirect=False, **params)
+  xd = sol_d['x']
+  yd = sol_d['y']
+  print('p*  = ', p_star)
+  print('pri error = ', (dot(data['c'], xd) - p_star) / p_star)
+  print('dual error = ', (-dot(data['b'], yd) - p_star) / p_star)
+
+
+if __name__ == '__main__':
+  main()
