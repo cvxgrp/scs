84290b31f0419730bf29449ae522cd34e86c45aa
Brendan O'Donoghue <bodonoghue85@gmail.com>
2017-10-23 15:47:11 +0100

    faster updates to matrix to invert

--- src/accel.c
+++ src/accel.c
@@ -1,6 +1,6 @@
-#include "scs.h"
 #include "accel.h"
 #include "linalg.h"
+#include "scs.h"
 #include "scs_blas.h"
 #include "util.h"
 
@@ -14,20 +14,21 @@
  * robust methods if we wanted to.
  */
 
-#define REGULARIZATION (0.0)
-
 struct SCS_ACCEL_WORK {
 #ifdef LAPACK_LIB_FOUND
   scs_float *d_f;
   scs_float *d_g;
   scs_float *d_x;
+  scs_float *delta_f;
+  scs_float *delta_x;
   scs_float *f;
   scs_float *g;
   scs_float *x;
   scs_float *sol;
   scs_float *scratch;
   scs_float *mat;
-  blas_int * ipiv;
+  scs_float *tmp;
+  blas_int *ipiv;
   scs_int k, l;
 #endif
   scs_float total_accel_time;
@@ -35,15 +36,39 @@ struct SCS_ACCEL_WORK {
 
 #ifdef LAPACK_LIB_FOUND
 void BLAS(gemv)(const char *trans, const blas_int *m, const blas_int *n,
-    const scs_float *alpha, const scs_float *a, const blas_int *lda,
-    const scs_float *x, const blas_int *incx, const scs_float *beta,
-    scs_float *y, const blas_int *incy);
-void BLAS(gemm)(const char *transa, const char *transb, blas_int *m, blas_int *
-    n, blas_int *k, scs_float *alpha, scs_float *a, blas_int *lda,
-    scs_float *b, blas_int *ldb, scs_float *beta, scs_float *c, blas_int
-    *ldc);
-void BLAS(gesv) (blas_int * n, blas_int * nrhs, scs_float * a,
-    blas_int * lda, blas_int * ipiv, scs_float * b, blas_int * ldb, blas_int * info);
+                const scs_float *alpha, const scs_float *a, const blas_int *lda,
+                const scs_float *x, const blas_int *incx, const scs_float *beta,
+                scs_float *y, const blas_int *incy);
+void BLAS(gesv)(blas_int *n, blas_int *nrhs, scs_float *a, blas_int *lda,
+                blas_int *ipiv, scs_float *b, blas_int *ldb, blas_int *info);
+
+void update_mat(ScsAccelWork *a, scs_int idx) {
+  /* use sol as scratch workspace here */
+  scs_int i;
+  scs_float *wrk = a->sol;
+  scs_float *d_f = a->d_f;
+  scs_float *d_x = a->d_x;
+  scs_float *delta_x = a->delta_x;
+  scs_float *delta_f = a->delta_f;
+  scs_float *mat = a->mat;
+  scs_int l = a->l;
+  scs_int k = a->k;
+  /* blas vars */
+  blas_int twol = 2 * l;
+  blas_int one = 1;
+  blas_int bk = (blas_int) a->k;
+  scs_float onef = 1.0;
+  scs_float zerof = 0.0;
+
+  scs_float ip = inner_prod(delta_x, delta_f, 2 * l);
+  BLAS(gemv)("Trans", &twol, &bk, &onef, d_x, &twol, delta_f, &one, &zerof, wrk, &one);
+  add_scaled_array(&(mat[idx * k]), wrk, k, -1.0);
+  BLAS(gemv)("Trans", &twol, &bk, &onef, d_f, &twol, delta_x, &one, &zerof, wrk, &one);
+  for (i = 0; i < k; ++i) {
+    mat[i * k + idx] -= wrk[i];
+  }
+  mat[idx * k + idx] += ip;
+}
 
 void update_accel_params(ScsWork *w, scs_int idx) {
   DEBUG_FUNC
@@ -53,28 +78,46 @@ void update_accel_params(ScsWork *w, scs_int idx) {
   scs_float *f = w->accel->f;
   scs_float *g = w->accel->g;
   scs_float *x = w->accel->x;
-  scs_int l = w->m + w->n + 1;
+  scs_float *delta_x = w->accel->delta_x;
+  scs_float *delta_f = w->accel->delta_f;
+  scs_int l = w->accel->l;
+
   /* copy g_prev into idx col of d_g */
   memcpy(&(d_g[idx * 2 * l]), g, sizeof(scs_float) * 2 * l);
-  /* copy f_prev into idx col of d_f */
-  memcpy(&(d_f[idx * 2 * l]), f, sizeof(scs_float) * 2 * l);
-  /* copy x_prev into idx col of d_x */
-  memcpy(&(d_x[idx * 2 * l]), x, sizeof(scs_float) * 2 * l);
-  /* x = [u_prev;v_prev] */
-  memcpy(x, w->u_prev, sizeof(scs_float) * l);
-  memcpy(&(x[l]), w->v_prev, sizeof(scs_float) * l);
+
+  /* copy old col d_f into delta_f */
+  memcpy(delta_f, &(d_f[idx * 2 * l]), sizeof(scs_float) * 2 * l);
+  /* copy old col d_x into delta_x */
+  memcpy(delta_x, &(d_x[idx * 2 * l]), sizeof(scs_float) * 2 * l);
+  /* delta_f -= f_prev */
+  add_scaled_array(delta_f, f, 2 * l, -1.0);
+  /* delta_x -= x_prev */
+  add_scaled_array(delta_x, x, 2 * l, -1.0);
+
   /* g = [u;v] */
   memcpy(g, w->u, sizeof(scs_float) * l);
   memcpy(&(g[l]), w->v, sizeof(scs_float) * l);
+  /* x = [u_prev;v_prev] */
+  memcpy(x, w->u_prev, sizeof(scs_float) * l);
+  memcpy(&(x[l]), w->v_prev, sizeof(scs_float) * l);
   /* calculcate f = g - x */
   memcpy(f, g, sizeof(scs_float) * 2 * l);
   add_scaled_array(f, x, 2 * l, -1.0);
+
+  /* delta_f += f */
+  add_scaled_array(delta_f, f, 2 * l, 1.0);
+  /* delta_x += x */
+  add_scaled_array(delta_x, x, 2 * l, 1.0);
+
+  /* update mat = d_x'*d_f using delta_x, delta_f */
+  update_mat(w->accel, idx);
+
   /* idx col of d_g = g_prev - g */
   add_scaled_array(&(d_g[idx * 2 * l]), g, 2 * l, -1);
-  /* idx col of d_f = f_prev - f */
-  add_scaled_array(&(d_f[idx * 2 * l]), f, 2 * l, -1);
-  /* idx col of d_x = x_prev - x */
-  add_scaled_array(&(d_x[idx * 2 * l]), x, 2 * l, -1);
+  /* idx col of d_f -= delta_f */
+  add_scaled_array(&(d_f[idx * 2 * l]), delta_f, 2 * l, -1);
+  /* idx col of d_x -= delta_x */
+  add_scaled_array(&(d_x[idx * 2 * l]), delta_x, 2 * l, -1);
   RETURN;
 }
 
@@ -98,48 +141,46 @@ ScsAccelWork *init_accel(ScsWork *w) {
   a->f = scs_calloc(2 * a->l, sizeof(scs_float));
   a->g = scs_calloc(2 * a->l, sizeof(scs_float));
   a->x = scs_calloc(2 * a->l, sizeof(scs_float));
+  a->delta_f = scs_calloc(2 * a->l, sizeof(scs_float));
+  a->delta_x = scs_calloc(2 * a->l, sizeof(scs_float));
   a->mat = scs_calloc(a->k * a->k, sizeof(scs_float));
+  a->tmp = scs_calloc(a->k * a->k, sizeof(scs_float));
   a->sol = scs_malloc(sizeof(scs_float) * 2 * a->l);
   a->scratch = scs_malloc(sizeof(scs_float) * a->k);
   a->ipiv = scs_malloc(sizeof(blas_int) * a->k);
   a->total_accel_time = 0.0;
-  if (!a->d_f || !a->d_g || !a->f || !a->g || !a->scratch || !a->sol || !a->d_x || !a->x || !a->scratch || !a->ipiv || !a->mat) {
+  if (!a->d_f || !a->d_g || !a->f || !a->g || !a->scratch || !a->sol ||
+      !a->d_x || !a->x || !a->scratch || !a->ipiv || !a->mat) {
     free_accel(a);
     a = SCS_NULL;
   }
   RETURN a;
 }
 
-scs_int solve_with_gesv(ScsAccelWork * a, scs_int len) {
+scs_int solve_with_gesv(ScsAccelWork *a, scs_int len) {
   DEBUG_FUNC
-  scs_int i;
   blas_int info;
   blas_int twol = 2 * a->l;
   blas_int one = 1;
-  blas_int blen = (blas_int) len;
+  blas_int blen = (blas_int)len;
   scs_float neg_onef = -1.0;
   scs_float onef = 1.0;
   scs_float zerof = 0.0;
-  scs_float * d_x = a->d_x;
-  scs_float * d_f = a->d_f;
-  scs_float regularization = REGULARIZATION;
-  if (regularization > 0.) {
-    memset(a->mat, 0, len * len * sizeof(scs_float));
-    for (i = 0; i < len; ++i) {
-      a->mat[i * len + i] = 1.0;
-    }
-  }
-  /* mat = dX'*dF */
-  BLAS(gemm)("Trans", "NoTrans", &blen, &blen, &twol, &onef, d_x, &twol, d_f, &twol, &regularization, a->mat, &blen);
+  scs_float *d_x = a->d_x;
+  scs_float *mat = a->mat;
+  scs_float *tmp = a->tmp;
   /* scratch = dX' f */
   BLAS(gemv)("Trans", &twol, &blen, &onef, d_x, &twol, a->f, &one, &zerof, a->scratch, &one);
+  /* copy mat into tmp since tmp is destroyed by gesv */
+  memcpy(tmp, mat, a->k * a->k * sizeof(scs_float));
   /* scratch = (dX'dF) \ dX' f */
-  BLAS(gesv)(&blen, &one, a->mat, &blen, a->ipiv, a->scratch, &blen, &info);
+  BLAS(gesv)(&blen, &one, tmp, &blen, a->ipiv, a->scratch, &blen, &info);
   /* sol = g */
   memcpy(a->sol, a->g, sizeof(scs_float) * 2 * a->l);
   /* sol = sol - dG * scratch */
-  BLAS(gemv)("NoTrans", &twol, &blen, &neg_onef, a->d_g, &twol, a->scratch, &one, &onef, a->sol, &one);
-  RETURN (scs_int) info;
+  BLAS(gemv)("NoTrans", &twol, &blen, &neg_onef, a->d_g, &twol, a->scratch, &one, &onef,
+   a->sol, &one);
+  RETURN(scs_int) info;
 }
 
 scs_int accelerate(ScsWork *w, scs_int iter) {
@@ -154,7 +195,7 @@ scs_int accelerate(ScsWork *w, scs_int iter) {
   }
   /* update df, d_g, d_x, f, g, x */
   update_accel_params(w, iter % k);
-  if (iter == 0) {
+  if (iter < k + 1) {
     RETURN 0;
   }
   /* solve linear system, new point stored in sol */
@@ -176,9 +217,12 @@ void free_accel(ScsAccelWork *a) {
     if (a->f) scs_free(a->f);
     if (a->g) scs_free(a->g);
     if (a->x) scs_free(a->x);
+    if (a->delta_f) scs_free(a->delta_f);
+    if (a->delta_x) scs_free(a->delta_x);
     if (a->sol) scs_free(a->sol);
     if (a->scratch) scs_free(a->scratch);
     if (a->mat) scs_free(a->mat);
+    if (a->tmp) scs_free(a->tmp);
     if (a->ipiv) scs_free(a->ipiv);
     scs_free(a);
   }
