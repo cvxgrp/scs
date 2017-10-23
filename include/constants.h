70f97b362b1ae84bb763b1e3c8fece2ccce41969
Brendan O'Donoghue <bodonoghue85@gmail.com>
2017-10-23 10:51:58 +0100

    fixg

--- include/constants.h
+++ include/constants.h
@@ -23,9 +23,9 @@ extern "C" {
 
 /* DEFAULT SOLVER PARAMETERS AND SETTINGS --------------------------    */
 #define MAX_ITERS (5000)
-#define EPS (1E-3)
+#define EPS (1E-6)
 #define ALPHA (1.5)
-#define RHO_X (1E-6)
+#define RHO_X (1E-3)
 #define SCALE (1.0)
 #define CG_RATE (2.0)
 #define VERBOSE (1)
