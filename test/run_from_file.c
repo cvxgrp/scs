1f51fa60db4ccbaab604bcf06b52215aefa0e6c6
Brendan O'Donoghue <bodonoghue85@gmail.com>
2018-12-26 17:51:34 +0000

    forgot to add files

---
+++ test/run_from_file.c
@@ -0,0 +1,23 @@
+#include "scs.h"
+#include "rw.h"
+
+/* Simple helper function to run problems from data files */
+/* Mostly useful for debugging */
+int main(int argc, char **argv) {
+    ScsData *d;
+    ScsCone *k;
+    ScsSolution *sol;
+    ScsInfo info = {0};
+    if (argc < 2) {
+      scs_printf("Need to specify a filename, exit.\n");
+      return -1;
+    }
+    char * filename = argv[1];
+    SCS(read_data)(filename, &d, &k);
+    sol = scs_calloc(1, sizeof(ScsSolution));
+    scs(d, k, sol, &info);
+    SCS(free_data)(d, k);
+    SCS(free_sol)(sol);
+    return 0;
+}
+
