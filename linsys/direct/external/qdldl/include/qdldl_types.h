55b9e1943691a8e4a3247cbd3dfd0290abc9e1fb
Brendan O'Donoghue <bodonoghue85@gmail.com>
2018-12-28 14:20:34 +0000

    checkin

---
+++ linsys/direct/external/qdldl/include/qdldl_types.h
@@ -0,0 +1,19 @@
+#ifndef QDLDL_TYPES_H
+# define QDLDL_TYPES_H
+
+# ifdef __cplusplus
+extern "C" {
+# endif /* ifdef __cplusplus */
+
+// QDLDL integer and float types
+
+#define QDLDL_int scs_int
+#define QDLDL_float scs_float
+#define QDLDL_bool scs_int
+
+# ifdef __cplusplus
+}
+# endif /* ifdef __cplusplus */
+
+#endif /* ifndef QDLDL_TYPES_H */
+
