61cba87db1ebf1805e3cda119e46285e5f454dc1
Brendan O'Donoghue <bodonoghue85@gmail.com>
2019-04-03 10:24:23 +0100

    Update private.c

--- linsys/gpu/private.c
+++ linsys/gpu/private.c
@@ -166,6 +166,8 @@ void SCS(free_lin_sys_work)(ScsLinSysWork *p) {
     }
     cusparseDestroy(p->cusparse_handle);
     cublasDestroy(p->cublas_handle);
+    /* Don't reset because it interferes with other GPU programs. */
+    /* cudaDeviceReset(); */
     scs_free(p);
   }
 }
