9e4d98d3d37e1752b74ffb4885a814fd8b38809f
Brendan O'Donoghue <bodonoghue85@gmail.com>
2019-11-05 12:49:25 +0000

    add sizeof helper functions

--- src/scs_version.c
+++ src/scs_version.c
@@ -1,3 +1,5 @@
 #include "glbopts.h"
 
 const char *SCS(version)(void) { return SCS_VERSION; }
+size_t SCS(sizeof_int)(void) { return sizeof(scs_int); }
+size_t SCS(sizeof_float)(void) { return sizeof(scs_float); }
