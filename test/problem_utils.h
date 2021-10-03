273d7419dc8139b8b3d8357600bc034bb40cd6dc
Brendan O'Donoghue <bodonoghue85@gmail.com>
2021-10-03 23:37:59 +0100

    tweak tolerances in test

--- test/problem_utils.h
+++ test/problem_utils.h
@@ -223,8 +223,8 @@ const char *verify_solution_correct(ScsData *d, ScsCone *k, ScsSettings *stgs,
     mu_assert_less("Dual obj ERROR", ABS(dobj - info->dobj), 1e-12);
     /* slightly looser tol */
     mu_assert_less("Complementary slackness ERROR", ABS(sty), 1e-6);
-    mu_assert_less("s cone dist ERROR", ABS(sdist), 1e-6);
-    mu_assert_less("y cone dist ERROR", ABS(ydist), 1e-6);
+    mu_assert_less("s cone dist ERROR", ABS(sdist), 1e-5);
+    mu_assert_less("y cone dist ERROR", ABS(ydist), 1e-5);
 
     mu_assert_less("Primal feas ERROR", res_pri,
                    stgs->eps_abs + stgs->eps_rel * prl);
@@ -235,14 +235,14 @@ const char *verify_solution_correct(ScsData *d, ScsCone *k, ScsSettings *stgs,
   } else if (status == SCS_INFEASIBLE) {
     mu_assert_less("Infeas ERROR", ABS(res_infeas - info->res_infeas), 1e-8);
     mu_assert_less("bty ERROR", ABS(bty + 1), 1e-12);
-    mu_assert_less("y cone dist ERROR", ABS(ydist), 1e-6);
+    mu_assert_less("y cone dist ERROR", ABS(ydist), 1e-5);
     mu_assert_less("Infeas invalid ERROR", res_infeas, stgs->eps_infeas);
 
   } else if (status == SCS_UNBOUNDED) {
     mu_assert_less("Unbdd_a ERROR", ABS(res_unbdd_a - info->res_unbdd_a), 1e-8);
     mu_assert_less("Unbdd_p ERROR", ABS(res_unbdd_p - info->res_unbdd_p), 1e-8);
     mu_assert_less("ctx ERROR", ABS(ctx + 1), 1e-12);
-    mu_assert_less("s cone dist ERROR", ABS(sdist), 1e-6);
+    mu_assert_less("s cone dist ERROR", ABS(sdist), 1e-5);
     mu_assert_less("Unbounded P invalid ERROR", res_unbdd_p, stgs->eps_infeas);
     mu_assert_less("Unbounded A invalid ERROR", res_unbdd_a, stgs->eps_infeas);
 
