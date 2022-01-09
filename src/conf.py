c17f5557de5d61c15256301cf9c25739b70e5af1
bodono <bodonoghue85@gmail.com>
2022-01-09 18:28:27 +0000

    Deploying to gh-pages from @ cvxgrp/scs@d542f3e08ecfd5946bfa8cc26f6f8d4c2d308e6a 🚀

--- src/conf.py
+++ src/conf.py
@@ -44,6 +44,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# sphinx pygments style uses ugly green boxes for code blocks
 #pygments_style = 'sphinx'
 pygments_style = 'default'
 
