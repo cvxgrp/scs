ea27ba2895e84510192603585d2c1eef1dc2fa82
Brendan O'Donoghue <bodonoghue85@gmail.com>
2022-01-09 15:54:52 +0000

    add examples to docs

---
+++ docs/src/examples/qp.m
@@ -0,0 +1,21 @@
+% First, make sure SCS is in the path so MATLAB can call it
+addpath("/Users/bodonoghue/git/scs-matlab")
+
+% Set up data
+data.P = sparse([3., -1.; -1., 2.]);
+data.A = sparse([-1., 1.; 1., 0.; 0., 1.]);
+data.b = [-1; 0.3; -0.5];
+data.c = [-1.; -1.];
+cone.l = size(data.b, 1);
+
+% Optional solver settings
+settings = struct('eps_abs', 1e-9, 'eps_rel', 1e-9);
+
+% Solve!
+[x, y, s, info] = scs(data, cone, settings);
+
+disp(sprintf("SCS took %i iters", info.iter))
+disp("Optimal solution vector x*:");
+disp(x)
+disp("Optimal dual vector y*:");
+disp(y)
