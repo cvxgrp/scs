49fef36cb1b1ec599f50b2851e581d7ef7390635
bodono <bodonoghue85@gmail.com>
2022-02-04 12:39:05 +0000

    Deploying to gh-pages from @ cvxgrp/scs@7d0ee41ebceba1c3219a2ecc1c0e6ca129a23c41 🚀

--- src/conf.py
+++ src/conf.py
@@ -19,7 +19,7 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'SCS'
+project = "SCS"
 copyright = "2021, Brendan O'Donoghue"
 author = "Brendan O'Donoghue"
 
@@ -34,25 +34,25 @@ version = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax', 'breathe', 'sphinx_rtd_theme']
+extensions = ["sphinx.ext.mathjax", "breathe", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # sphinx pygments style uses ugly green boxes for code blocks
-#pygments_style = 'sphinx'
-pygments_style = 'default'
+# pygments_style = 'sphinx'
+pygments_style = "default"
 
-#html_sidebars = {
+# html_sidebars = {
 #   '**': [
 #       'about.html', 'navigation.html', 'searchbox.html',
 #   ]
-#}
+# }
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -60,17 +60,19 @@ pygments_style = 'default'
 # a list of builtin themes.
 
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-def setup(app):
-    app.add_css_file('css/scs_theme.css')
 
-html_logo = '_static/scs_logo_transparent.png'
-html_favicon = '_static/favicon.ico'
+def setup(app):
+    app.add_css_file("css/scs_theme.css")
+
+
+html_logo = "_static/scs_logo_transparent.png"
+html_favicon = "_static/favicon.ico"
 html_theme_options = {
-    'logo_only': True,
-    'display_version': True,
+    "logo_only": True,
+    "display_version": True,
     #'github_banner': True,
     #'github_user': 'cvxgrp',
     #'github_repo': 'scs',
@@ -78,13 +80,13 @@ html_theme_options = {
     #'logo_name': False,
     #'github_button': False,
     #'github_type': 'star',
-    'analytics_id': 'UA-203326834-1',
+    "analytics_id": "UA-203326834-1",
 }
 
-rst_epilog = '.. |version| replace:: %s' % __version__ 
+rst_epilog = ".. |version| replace:: %s" % __version__
 
 # Breathe docs
-subprocess.call('doxygen Doxyfile', shell=True)
+subprocess.call("doxygen Doxyfile", shell=True)
 
 breathe_projects = {"scs": "doxygen_out/xml/"}
 breathe_default_project = "scs"
@@ -92,4 +94,4 @@ breathe_default_project = "scs"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
