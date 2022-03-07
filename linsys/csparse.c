6959df9cc3d7a7998fbbea978e5f59ec01180bd4
Brendan O'Donoghue <bodonoghue85@gmail.com>
2022-03-07 16:02:06 +0000

    fix merge conflict

--- linsys/csparse.c
+++ linsys/csparse.c
@@ -102,7 +102,7 @@ ScsMatrix *SCS(form_kkt)(const ScsMatrix *A, const ScsMatrix *P,
    *
    */
   scs_int h, i, j, count;
-  ScsMatrix *KScsMatrix, *K;
+  ScsMatrix *Kcsc, *K;
   scs_int n = A->n;
   scs_int m = A->m;
   scs_int Anz = A->p[n];
@@ -205,137 +205,7 @@ ScsMatrix *SCS(form_kkt)(const ScsMatrix *A, const ScsMatrix *P,
   }
 
   idx_mapping = (scs_int *)scs_calloc(count, sizeof(scs_int));
-  KScsMatrix = SCS(cs_compress)(K, count, idx_mapping);
-  for (i = 0; i < m + n; i++) {
-    diag_r_idxs[i] = idx_mapping[diag_r_idxs[i]];
-  }
-  SCS(cs_spfree)(K);
-  scs_free(idx_mapping);
-  return KScsMatrix;
-}
-
-/* Build the KKT matrix */
-csc *SCS(form_kkt)(const ScsMatrix *A, const ScsMatrix *P, scs_float *diag_p,
-                   const scs_float *diag_r, scs_int *diag_r_idxs,
-                   scs_int upper) {
-  /*
-   * Forms column compressed KKT matrix assumes column compressed A,P matrices.
-   * Only upper OR lower triangular part is stuffed, depending on `upper` flag.
-   *
-   * Forms upper/lower triangular part of [(R_x + P)  A'; A -R_y]
-   * Shapes: P : n x n, A: m x n.
-   *
-   * Output: `diag_p` will contain values of P diagonal upon completion,
-   * and `diag_r_idxs` will contain the indices corresponding to the entries
-   * in the returned matrix corresponding to the entries of R.
-   *
-   */
-  scs_int h, i, j, count;
-  csc *Kcsc, *K;
-  scs_int n = A->n;
-  scs_int m = A->m;
-  scs_int Anz = A->p[n];
-  scs_int Knzmax;
-  scs_int *idx_mapping;
-  if (P) {
-    /* Upper bound P + I triangular component NNZs as Pnz + n */
-    Knzmax = n + m + Anz + P->p[n];
-  } else {
-    Knzmax = n + m + Anz;
-  }
-  K = SCS(cs_spalloc)(m + n, m + n, Knzmax, 1, 1);
-
-#if VERBOSITY > 0
-  scs_printf("forming kkt\n");
-#endif
-  /* Here we generate a triplet matrix and then compress to CSC */
-  if (!K) {
-    return SCS_NULL;
-  }
-
-  count = 0; /* element counter */
-  if (P) {
-    /* R_x + P in top left */
-    for (j = 0; j < n; j++) { /* cols */
-      diag_p[j] = 0.;
-      /* empty column, add diagonal  */
-      if (P->p[j] == P->p[j + 1]) {
-        K->i[count] = j;
-        K->p[count] = j;
-        K->x[count] = diag_r[j];
-        diag_r_idxs[j] = count; /* store the indices where diag_r occurs */
-        count++;
-      }
-      for (h = P->p[j]; h < P->p[j + 1]; h++) {
-        i = P->i[h]; /* row */
-        if (i > j) { /* only upper triangular needed */
-          break;
-        }
-        if (upper) {
-          K->i[count] = i;
-          K->p[count] = j;
-        } else { /* lower triangular */
-          /* P is passed in upper triangular, need to flip that here */
-          K->i[count] = j; /* col -> row */
-          K->p[count] = i; /* row -> col */
-        }
-        K->x[count] = P->x[h];
-        if (i == j) {
-          /* P has diagonal element */
-          diag_p[j] = P->x[h];
-          K->x[count] += diag_r[j];
-          diag_r_idxs[j] = count; /* store the indices where diag_r occurs */
-        }
-        count++;
-        /* reached the end without adding diagonal, do it now */
-        if ((i < j) && (h + 1 == P->p[j + 1] || P->i[h + 1] > j)) {
-          K->i[count] = j;
-          K->p[count] = j;
-          K->x[count] = diag_r[j];
-          diag_r_idxs[j] = count; /* store the indices where diag_r occurs */
-          count++;
-        }
-      }
-    }
-  } else {
-    /* R_x in top left */
-    for (j = 0; j < n; j++) {
-      diag_p[j] = 0.;
-      K->i[count] = j;
-      K->p[count] = j;
-      K->x[count] = diag_r[j];
-      diag_r_idxs[j] = count; /* store the indices where diag_r occurs */
-      count++;
-    }
-  }
-
-  /* A in bottom left or A^T top right */
-  for (j = 0; j < n; j++) { /* column */
-    for (h = A->p[j]; h < A->p[j + 1]; h++) {
-      if (upper) {
-        K->p[count] = A->i[h] + n; /* column */
-        K->i[count] = j;           /*row */
-      } else {                     /* lower triangular */
-        K->p[count] = j;           /* column */
-        K->i[count] = A->i[h] + n; /* row */
-      }
-      K->x[count] = A->x[h];
-      count++;
-    }
-  }
-
-  /* -R_y at bottom right */
-  for (j = 0; j < m; j++) {
-    K->i[count] = j + n;
-    K->p[count] = j + n;
-    K->x[count] = -diag_r[j + n];
-    diag_r_idxs[j + n] = count; /* store the indices where diag_r occurs */
-    count++;
-  }
-
-  K->nz = count;
-  idx_mapping = (scs_int *)scs_calloc(K->nz, sizeof(scs_int));
-  Kcsc = SCS(cs_compress)(K, idx_mapping);
+  Kcsc = SCS(cs_compress)(K, count, idx_mapping);
   for (i = 0; i < m + n; i++) {
     diag_r_idxs[i] = idx_mapping[diag_r_idxs[i]];
   }
