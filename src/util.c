8bdf16f891955db6619774492bfcccd1cda60269
Brendan O'Donoghue <bodonoghue85@gmail.com>
2023-03-22 18:31:45 +0000

    cleanup assuming sizeof(char) = 1

--- src/util.c
+++ src/util.c
@@ -86,10 +86,8 @@ void SCS(deep_copy_stgs)(ScsSettings *dest, const ScsSettings *src) {
   memcpy(dest, src, sizeof(ScsSettings));
   /* MATLAB does something weird with strdup, so use strcpy instead */
   char *tmp;
-  scs_int len;
   if (src->write_data_filename) {
-    len = strlen(src->write_data_filename) + 1;
-    tmp = (char *)scs_calloc(len, sizeof(char));
+    tmp = (char *)scs_calloc(strlen(src->write_data_filename) + 1);
     strcpy(tmp, src->write_data_filename);
     dest->write_data_filename = tmp;
   } else {
@@ -97,8 +95,7 @@ void SCS(deep_copy_stgs)(ScsSettings *dest, const ScsSettings *src) {
   }
   /* MATLAB does something weird with strdup, so use strcpy instead */
   if (src->log_csv_filename) {
-    len = strlen(src->log_csv_filename) + 1;
-    tmp = (char *)scs_calloc(len, sizeof(char));
+    tmp = (char *)scs_calloc(strlen(src->log_csv_filename) + 1);
     strcpy(tmp, src->log_csv_filename);
     dest->log_csv_filename = tmp;
   } else {
