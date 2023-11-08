9024b8ccc1bba6ee797440fb22354cadb9c81839
Brendan O'Donoghue <bodonoghue85@gmail.com>
2023-11-08 11:20:07 +0000

    bump to 3.2.4

--- include/glbopts.h
+++ include/glbopts.h
@@ -14,7 +14,7 @@ extern "C" {
 
 /* SCS VERSION NUMBER ----------------------------------------------     */
 /* string literals automatically null-terminated */
-#define SCS_VERSION ("3.2.3")
+#define SCS_VERSION ("3.2.4")
 
 /* verbosity level */
 #ifndef VERBOSITY
