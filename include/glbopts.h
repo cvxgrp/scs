89ae068f5ac449c1cd75808d841b94a52c3450ff
Brendan O'Donoghue <bodonoghue85@gmail.com>
2024-06-27 10:44:29 -0600

    bump to 3.2.5 due to blas issue fix

--- include/glbopts.h
+++ include/glbopts.h
@@ -14,7 +14,7 @@ extern "C" {
 
 /* SCS VERSION NUMBER ----------------------------------------------     */
 /* string literals automatically null-terminated */
-#define SCS_VERSION ("3.2.4")
+#define SCS_VERSION ("3.2.5")
 
 /* verbosity level */
 #ifndef VERBOSITY
