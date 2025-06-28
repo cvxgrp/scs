ad07316af397d6d0114ddab86eeddf3d98441e67
Daniel <danielcederberg1@gmail.com>
2025-06-28 14:16:36 -0700

    complex PSD cone

--- include/scs_types.h
+++ include/scs_types.h
@@ -7,11 +7,7 @@
 #ifndef SCS_TYPES_H_GUARD
 #define SCS_TYPES_H_GUARD
 
-#ifdef __cplusplus
-#include <complex> // Use C++ <complex> instead of C <complex.h> for C++ code
-#else
-#include <complex.h> // Use C <complex.h> for C code
-#endif
+#include <complex.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,18 +28,10 @@ typedef int scs_int;
 
 #ifndef SFLOAT
 typedef double scs_float;
-#ifdef __cplusplus
-typedef std::complex<double> scs_complex_float;
-#else
 typedef double _Complex scs_complex_float;
-#endif
 #else
 typedef float scs_float;
-#ifdef __cplusplus
-typedef std::complex<float> scs_complex_float;
-#else
 typedef float _Complex scs_complex_float;
-#endif
 #endif
 
 #ifdef __cplusplus
