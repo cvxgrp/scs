4f6398b9ced921151f864dc008a1227fc782c2dd
Daniel <danielcederberg1@gmail.com>
2025-06-28 15:12:43 -0700

    as it used to be

--- include/scs_types.h
+++ include/scs_types.h
@@ -1,14 +1,26 @@
+/*
+ * Public header including definitions of primitive types used in SCS.
+ * Make sure this file and `scs.h` are somewhere appropriate and then use
+ * `#include "scs.h"` to access the SCS public API.
+ */
+
 #ifndef SCS_TYPES_H_GUARD
 #define SCS_TYPES_H_GUARD
 
 #ifdef __cplusplus
-#  include <complex>   // Use C++ complex type
 extern "C" {
-#else
-#  include <complex.h> // Use C complex.h
 #endif
 
+#include <complex.h>
+
 #ifdef DLONG
+/*#ifdef _WIN64
+#include <stdint.h>
+typedef int64_t scs_int;
+#else
+typedef long scs_int;
+#endif
+*/
 typedef long long scs_int;
 #else
 typedef int scs_int;
@@ -16,18 +28,10 @@ typedef int scs_int;
 
 #ifndef SFLOAT
 typedef double scs_float;
-#  ifdef __cplusplus
-typedef std::complex<scs_float> scs_complex_float;
-#  else
 typedef double _Complex scs_complex_float;
-#  endif
 #else
 typedef float scs_float;
-#  ifdef __cplusplus
-typedef std::complex<scs_float> scs_complex_float;
-#  else
 typedef float _Complex scs_complex_float;
-#  endif
 #endif
 
 #ifdef __cplusplus
