36d7c7e0876b0c6faa58a5d4879515dca6f83e51
bodono <bodonoghue85@gmail.com>
2026-09-08 08:19:46 +0000

    Deploying to gh-pages from @ cvxgrp/scs@9c3b44ee9ce5cccf1f4c5d939e761c988a977650 🚀

--- src/conf.py
+++ src/conf.py
@@ -58,7 +58,6 @@ pygments_style = "default"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-# html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 
 
